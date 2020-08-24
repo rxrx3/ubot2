43e6a0201abb27ca1b5ab5f90643e43f09be80ef
rxrx3 <70162537+rxrx3@users.noreply.github.com>
2020-08-24 20:31:29 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,17 +8,14 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/157bae87162219b1a9b77.png"
 pm_caption = "**Userbot di** @GucciRxnS **ONLINE ✅**\n\n"
-pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "**⚙ STATO SISTEMA**\n"
+pm_caption += "`TELETHON VERSION:` **6.0.9**\n🐍 `Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
 pm_caption += "**Current Branch** : `master`\n"
 pm_caption += "**Friday OS** : `3.14`\n"
 pm_caption += "**Current Sat** : `StarkGangSat-2.25`\n"
 pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "Copyright : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "**Heroku Database** : `AWS - Working Properly`"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
