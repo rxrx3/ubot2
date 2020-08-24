835009b3550eb63503f3ce908f1f81e5a5e95127
rxrx3 <70162537+rxrx3@users.noreply.github.com>
2020-08-24 18:53:13 +0200

    ddddd
    ddd

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,37 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+#IMG CREDITS: @WhySooSerious
+import asyncio
+from telethon import events
+from uniborg.util import admin_cmd
+from userbot import ALIVE_NAME
+from telethon.tl.types import ChannelParticipantsAdmins
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = "https://telegra.ph/file/157bae87162219b1a9b77.png"
+pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
+pm_caption += "**SYSTEM STATUS**\n"
+pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "`DATABASE STATUS:` **Functional**\n"
+pm_caption += "**Current Branch** : `master`\n"
+pm_caption += "**Friday OS** : `3.14`\n"
+pm_caption += "**Current Sat** : `StarkGangSat-2.25`\n"
+pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
+pm_caption += "Copyright : By [StarkGang@Github](GitHub.com/StarkGang)\n"
+pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+
+@borg.on(admin_cmd(pattern=r"alive"))
+async def friday(alive):
+    chat = await alive.get_chat()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete()
+
+    
+@borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
+async def friday(alive):
+    chat = await alive.get_chat()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+
+    
